fa9d7126ec41642f5ea63228f12033686f31166b
mivalt.filip <mivalt.filip@gmail.com>
2024-12-11 14:16:17 -0600

    Update workflows

--- brainmaze_utils/__init__.py
+++ brainmaze_utils/__init__.py
@@ -4,7 +4,7 @@ Brainmaze utils (:mod:`brainmaze_utils`)
 =======================================
 
 """
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 from brainmaze_utils import files
 from brainmaze_utils import signal
